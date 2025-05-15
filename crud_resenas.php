c53bce508351322996196125504aa78a5878a163
Diego <diemcoder@gmail.com>
2025-05-15 18:25:51 -0500

    af

--- crud_resenas.php
+++ crud_resenas.php
@@ -2,10 +2,10 @@
 include 'db_connection.php';
 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json; charset=UTF-8");
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
